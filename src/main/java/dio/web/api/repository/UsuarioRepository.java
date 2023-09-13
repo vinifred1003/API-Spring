15b04c3b3d68248387fa1e91fa039649a85a916a
needd <needd@LAPTOP-JES5LQ24>
2023-09-13 19:15:21 -0300

    First export

---
+++ src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -0,0 +1,45 @@
+package dio.web.api.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import dio.web.api.model.Usuario;
+
+@Repository
+public class UsuarioRepository {
+	public void save(Usuario usuario) {
+		if (usuario.getId() == null)
+			System.out.println("SAVE - Recebendo usuario na camada de repositório");
+		else
+			System.out.println("UPDATE - Recebendo usuario na camada de repositório");
+
+		System.out.println(usuario);
+	}
+
+	public void deleteById(Integer id) {
+		System.out.println(String.format("DELETE/id - Recebendo o id: %d para exclusão de um Usuario", id));
+
+		System.out.println(id);
+	}
+
+	public List<Usuario> findAll() {
+		System.out.println("LIST - listando usuarios do sistema");
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("vini", "123456"));
+		usuarios.add(new Usuario("jao", "senhaDoJao"));
+		return usuarios;
+	}
+
+	public Usuario findById(Integer id) {
+		System.out.println(String.format("FIND/id - Recebendo o id: %d para procura de um usuario", id));
+		return new Usuario("vini", "123456");
+	}
+
+	public Usuario findByUsername(String username) {
+		System.out.println(
+				String.format("FIND/username - Recebendo o username: %s para procura de um usuario", username));
+		return new Usuario("vini", "123456");
+	}
+}
